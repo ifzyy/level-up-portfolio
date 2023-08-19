4e16477995126f44c43d8c3cd74c960e8e2d8f5d
Johnson Emmanuel <johnsonnifemi8@gmail.com>
2023-08-19 23:31:28 +0100

    update app

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,10 +1,46 @@
 
-
+import Image from "next/image"
+import reactImage from "@/images/react.png"
+import nodeImage from "@/images/node.png"
+import mongoImage from "@/images/mongo.png"
+import reduxImage from "@/images/redux.png"
+import nextjsImage from "@/images/nextjs.png"
+import javascriptImage from "@/images/javascript.png"
+import cssImage from "@/images/css.png"
+import expressImage from "@/images/express.png"
+import vector30 from "@/images/vector 30.png"
+import vector29 from "@/images/vector 29.png"
+import vector28 from "@/images/vector 28.png"
+import vector27 from "@/images/vector 27.png"
+import vector26 from "@/images/vector 26.png"
+import vector25 from "@/images/vector 25.png"
 function Services() {
   return (
     <div>
-          <p className="text-right mr-96 mt-36" style={{ fontFamily: "Sevillana, cursive", fontSize: "30px"}}>I'm currently looking to join a <span style={{ color: "#7127BA;" }}>cross-functional</span>  team <br />
-              <span style={{ fontFamily: "Sevillana, cursive", fontSize: "25px" }}>that values improving people's lives through accessible design</span>  </p>
+      <p className="text-right mr-96 mt-36" style={{ fontFamily: "Sevillana, cursive", fontSize: "30px" }}>I'm currently looking to join a <span style={{ color: "#7127BA" }}>cross-functional</span>  team <br />
+        <span style={{ fontFamily: "Sevillana, cursive", fontSize: "25px" }}>that values improving people's lives through accessible design</span>  </p>
+
+        <ul className="flex justify-center items-center gap-4">
+        <li className="p-4 rounded-full bg-[#251C31]"><Image  src={ reactImage}/></li>
+        <li className="p-4 rounded-full bg-[#251C31]"><Image src={nodeImage } /></li>
+        <li className="p-4 rounded-full bg-[#251C31] "><Image src={mongoImage } /></li>
+        <li className="p-4 rounded-full bg-[#251C31]"><Image src={reduxImage } /></li>
+        <li className="p-4 rounded-full bg-[#251C31]"><Image src={nextjsImage } /></li>
+        <li className="p-4 rounded-full bg-[#251C31]"><Image src={ javascriptImage} /></li>
+        <li className="p-4 rounded-full bg-[#251C31]"><Image src={ cssImage} /></li>
+        <li className="p-4 rounded-full bg-[#251C31]"><Image  src={expressImage}/></li>    
+        </ul>
+
+      <ul className="flex justify-center items-center text-center  mt-4">
+
+        <li className="flex flex-col justify-center items-center text-center "><div className="p-4 rounded-full bg-[#251C31] mr-[100px] mt-[20px]"><Image src={reactImage} /></div>  <div className="mt-[-10px] z-[-10]"><Image src={vector28} /></div></li>
+        <li className="flex flex-col justify-center items-center"><div className="p-4 rounded-full bg-[#251C31] ml-[-50px] mt-[20px]"><Image src={reactImage} /></div>  <div className="mt-[-10px] z-[-10] "><Image src={vector29} /></div></li>
+        <li className="flex flex-col justify-center items-center"><div className="p-4 rounded-full bg-[#251C31]  mr-[20px] mt-[20px]"><Image src={reactImage} /></div>  <div className="mt-[-10px] z-[-10]"><Image src={vector30} /></div></li>
+        <li className="flex flex-col justify-center items-center"><div className="p-4 rounded-full bg-[#251C31]  ml-[40px] mt-[20px]"><Image src={reactImage} /></div>  <div className="mt-[-10px] z-[-10]"><Image src={vector27} /></div></li>
+        <li className="flex flex-col justify-center items-center"><div className="p-4 rounded-full bg-[#251C31]  mr-[-50px] mt-[20px]"><Image src={reactImage} /></div>  <div className="mt-[-10px] z-[-10]"><Image src={vector26} /></div></li>
+        <li className="flex flex-col justify-center items-center"><div className="p-4 rounded-full bg-[#251C31]  mr-[-110px] mt-[20px]"><Image src={reactImage} /></div>  <div className="mt-[-10px] z-[-10]"><Image src={vector25} /></div></li>
+        </ul>
+
     </div>
   )
 }
--- src/components/Work.js
+++ src/components/Work.js
@@ -4,8 +4,17 @@ function Work() {
   return (
     <div className="glow">
       <h2 style={{ marginLeft: "20%", fontFamily: "Sevillana,Cursive" }} className='text-6xl mt-36'>Work experience</h2>
-      <ul style={{ marginLeft: "20%" }} className='grid  gap-6'>
+      <ul style={{ marginLeft: "20%" }} className='grid  grid-cols-2 gap-6'>
         <li className='list__item flex items-center gap-8  p-5'><Image width="100" src={logo} /><div> <h2 className="" style={{ fontFamily: "Sevillana, cursive", fontSize: "33px", lineHeight: "40px", fontWeight: "bold" }}>Mentor</h2> <p style={{ fontFamily: "Sevillana, cursive", fontSize: "15px" }}>Proposed improvements to a lot of code organizations to improve code quality and overall
+          performance.</p>
+          <button className="learn__more">Learn more</button></div></li>
+        <li className='list__item flex items-center gap-8  p-5'><Image width="100" src={logo} /><div> <h2 className="" style={{ fontFamily: "Sevillana, cursive", fontSize: "33px", lineHeight: "40px", fontWeight: "bold" }}>Fullstack developer</h2> <p style={{ fontFamily: "Sevillana, cursive", fontSize: "15px" }}>Proposed improvements to a lot of code organizations to improve code quality and overall
+          performance.</p>
+          <button className="learn__more">Learn more</button></div></li>
+        <li className='list__item flex items-center gap-8  p-5'><Image width="100" src={logo} /><div> <h2 className="" style={{ fontFamily: "Sevillana, cursive", fontSize: "33px", lineHeight: "40px", fontWeight: "bold" }}>Mathematics Tutor</h2> <p style={{ fontFamily: "Sevillana, cursive", fontSize: "15px" }}>Proposed improvements to a lot of code organizations to improve code quality and overall
+          performance.</p>
+          <button className="learn__more">Learn more</button></div></li>
+        <li className='list__item flex items-center gap-8  p-5'><Image width="100" src={logo} /><div> <h2 className="" style={{ fontFamily: "Sevillana, cursive", fontSize: "33px", lineHeight: "40px", fontWeight: "bold" }}>Music Director</h2> <p style={{ fontFamily: "Sevillana, cursive", fontSize: "15px" }}>Proposed improvements to a lot of code organizations to improve code quality and overall
           performance.</p>
           <button className="learn__more">Learn more</button></div></li>
       </ul>
